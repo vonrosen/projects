2be97e140ea68e892470421f8329313cd232a6fb
Hunter Stern <vonrosen2000@yahoo.com>
2019-07-14 18:42:36 -0700

    more changes

--- src/org/hunter/hackerrank/ArrayManip.java
+++ src/org/hunter/hackerrank/ArrayManip.java
@@ -4,37 +4,65 @@ import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Arrays;
-import java.util.List;
 import java.util.Scanner;
-import java.util.stream.Collectors;
 
 public class ArrayManip {
 
     // Complete the arrayManipulation function below.
     static long arrayManipulation(int n, int[][] queries) {
     		long biggestSum = 0;
-    		
+    		long [][] sums = new long[queries.length][4];
+
     		for (int i = 0; i < queries.length; ++i) {
-    			long sum = 0;
-    			final int currentIndex = i;
-//    			List<int []> intList = Arrays.stream(queries).collect(Collectors.toList());
-//    			intList.remove(currentIndex);
-//    			sum = intList.stream().filter(item -> 
-//    				item[0] >= queries[currentIndex][0] && item[0] <= queries[currentIndex][1] ||
-//    				item[1] >= queries[currentIndex][0] && item[1] <= queries[currentIndex][1]).mapToInt(item -> item[2]).sum();
-    			
-    			sum = Arrays.stream(queries).filter(item -> 
-    				item[0] >= queries[currentIndex][0] && item[0] <= queries[currentIndex][1] ||
-    				item[1] >= queries[currentIndex][0] && item[1] <= queries[currentIndex][1]).mapToInt(item -> item[2]).sum();
-    			
-    			System.out.println(sum);
-    			
-    			if (sum > biggestSum) {
-    				biggestSum = sum;
-    			}    			
+//    			long sum1 = 0;
+//    			long sum2 = 0;
+//    			final int currentIndex = i;
+
+    			//System.out.println("array= " + queries[currentIndex][0] + "," + queries[currentIndex][1] + "," + queries[currentIndex][2]);
+
+//    			sum1 = Arrays.stream(queries).filter(item ->
+//    			    queries[currentIndex][0] >= item[0] && queries[currentIndex][0] <= item[1]).mapToInt(item -> item[2]).sum();
+//
+//                sum2 = Arrays.stream(queries).filter(item ->
+//                    queries[currentIndex][1] >= item[0] && queries[currentIndex][1] <= item[1]).mapToInt(item -> item[2]).sum();
+//
+//                sum1 = sum2 > sum1 ? sum2 : sum1;
+//
+//                biggestSum = sum1 > biggestSum ? sum1 : biggestSum;
+
+
+    		    if (i > 0) {
+    		        long sum = queries[i][2];
+    		        for (int j = 0; j < i; ++j) {
+    		            if (queries[i][0] >= sums[j][0] && queries[i][0] <= sums[j][1] ||
+    		                queries[i][1] >= sums[j][0] && queries[i][1] <= sums[j][1] ||
+    		                queries[i][0] < sums[j][0] && queries[i][1] > sums[j][1]) {
+
+    		                sum += sums[j][2];
+    		            }
+    		        }
+
+                    sums[i][0] = queries[i][0];
+                    sums[i][1] = queries[i][1];
+                    sums[i][2] = queries[i][2];
+                    sums[i][3] = sum;
+                    //System.out.println("it" + i + "=" + sum);
+    		    }
+    		    else {
+    		        sums[i][0] = queries[i][0];
+    		        sums[i][1] = queries[i][1];
+    		        sums[i][2] = queries[i][2];
+    		        sums[i][3] = queries[i][2];
+    		    }
     		}
 
-    		System.out.println(biggestSum);
+//    		for (int i = 0; i < sums.length; ++i) {
+//                System.out.println(sums[i][3]);
+//    		}
+
+    		biggestSum = Arrays.stream(sums).mapToLong(item -> item[3]).max().getAsLong();
+
+    		System.out.println("bs" + biggestSum);
     		return biggestSum;
     }
 
