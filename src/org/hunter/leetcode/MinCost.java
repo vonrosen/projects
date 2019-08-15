c4b32bd7ecbf0aedb43b73582c807c72e950a45f
Hunter Stern <vonrosen2000@yahoo.com>
2019-08-14 20:54:06 -0700

    more changes

--- src/org/hunter/leetcode/MinCost.java
+++ src/org/hunter/leetcode/MinCost.java
@@ -1,35 +1,158 @@
 package org.hunter.leetcode;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MinCost {
 
     public static void main(String [] args) {
         MinCost m = new MinCost();
         System.out.println(m.mincostTickets(new int [] {1,4,6,7,8,20} , new int [] {2,7,15}));
+        System.out.println(m.mincostTickets(new int [] {1,2,3,4,5,6,7,8,9,10,30,31} , new int [] {2,7,15}));
+        System.out.println(m.mincostTickets(new int [] {1,2,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,20,21,24,25,27,28,29,30,31,34,37,38,39,41,43,44,45,47,48,49,54,57,60,62,63,66,69,70,72,74,76,78,80,81,82,83,84,85,88,89,91,93,94,97,99}, new int [] {9,38,134}));
+
+    }
+
+    static class Node {
+        int dayPurchased;
+        int totalCost;
+        int type;
+        Node one;
+        Node seven;
+        Node thirty;
+
+        @Override
+        public String toString() {
+            return "Day purchased: " + dayPurchased + " Type: " + type + " Total cost: " + totalCost;
+        }
     }
 
     public int mincostTickets(int[] days, int[] costs) {
-        Map<Integer, Integer> dayToCost = new HashMap<Integer, Integer>();
+        Node root = new Node();
+        root.type = 1;
 
         for (int day : days) {
-            for (int i = 0; i < costs.length; ++i) {
-                if (dayToCost.get(day) == null) {
-                    if (i == 0) {
-                        dayToCost.put(day + 1, costs[i]);
-                    }
-                    else if (i == 1) {
+            List<Node> leafs = new ArrayList<Node>();
+            getLeafs(root, leafs);
 
+            for (Node leaf: leafs) {
+                if (leaf.type == 1 && leaf.dayPurchased + 1 <= day) {
+                    if (leaf.one == null) {
+                        leaf.one = new Node();
+                        leaf.one.type = 1;
+                        leaf.one.dayPurchased = day;
+                        leaf.one.totalCost = leaf.totalCost + costs[0];
                     }
-                    else {
 
+                    if (leaf.seven == null) {
+                        leaf.seven = new Node();
+                        leaf.seven.type = 7;
+                        leaf.seven.dayPurchased = day;
+                        leaf.seven.totalCost = leaf.totalCost + costs[1];
+                    }
+
+                    if (leaf.thirty == null) {
+                        leaf.thirty = new Node();
+                        leaf.thirty.type = 30;
+                        leaf.thirty.dayPurchased = day;
+                        leaf.thirty.totalCost = leaf.totalCost + costs[2];
                     }
                 }
 
+                if (leaf.type == 7 && leaf.dayPurchased + 7 <= day) {
+                    if (leaf.one == null) {
+                        leaf.one = new Node();
+                        leaf.one.type = 1;
+                        leaf.one.dayPurchased = day;
+                        leaf.one.totalCost = leaf.totalCost + costs[0];
+                    }
 
+                    if (leaf.seven == null) {
+                        leaf.seven = new Node();
+                        leaf.seven.type = 7;
+                        leaf.seven.dayPurchased = day;
+                        leaf.seven.totalCost = leaf.totalCost + costs[1];
+                    }
+
+                    if (leaf.thirty == null) {
+                        leaf.thirty = new Node();
+                        leaf.thirty.type = 30;
+                        leaf.thirty.dayPurchased = day;
+                        leaf.thirty.totalCost = leaf.totalCost + costs[2];
+                    }
+                }
+
+                if (leaf.type == 30 && leaf.dayPurchased + 30 <= day) {
+                    if (leaf.one == null) {
+                        leaf.one = new Node();
+                        leaf.one.type = 1;
+                        leaf.one.dayPurchased = day;
+                        leaf.one.totalCost = leaf.totalCost + costs[0];
+                    }
+
+                    if (leaf.seven == null) {
+                        leaf.seven = new Node();
+                        leaf.seven.type = 7;
+                        leaf.seven.dayPurchased = day;
+                        leaf.seven.totalCost = leaf.totalCost + costs[1];
+                    }
+
+                    if (leaf.thirty == null) {
+                        leaf.thirty = new Node();
+                        leaf.thirty.type = 30;
+                        leaf.thirty.dayPurchased = day;
+                        leaf.thirty.totalCost = leaf.totalCost + costs[2];
+                    }
+                }
             }
         }
 
+        return getMinCost(root);
+    }
+
+    private int getMinCost(Node node) {
+        List<Node> leafs = new ArrayList<Node>();
+
+        getLeafsForCost(node, leafs);
+
+        int minCost = 0;
+        for (Node leaf : leafs) {
+            if (minCost == 0) {
+                minCost = leaf.totalCost;
+            }
+            else {
+                minCost = leaf.totalCost < minCost ? leaf.totalCost : minCost;
+            }
+        }
+
+        return minCost;
+    }
+
+    private void getLeafsForCost(Node node, List<Node> leafs) {
+        if (node == null) {
+            return;
+        }
+
+        if (node.one == null && node.seven == null && node.thirty == null) {
+            leafs.add(node);
+        }
+
+        getLeafsForCost(node.one, leafs);
+        getLeafsForCost(node.seven, leafs);
+        getLeafsForCost(node.thirty, leafs);
+    }
+
+    void getLeafs(Node node, List<Node> leafs) {
+        if (node == null) {
+            return;
+        }
+
+        if (node.one == null || node.seven == null || node.thirty == null) {
+            leafs.add(node);
+        }
+
+        getLeafs(node.one, leafs);
+        getLeafs(node.seven, leafs);
+        getLeafs(node.thirty, leafs);
     }
 }
