4fcc5c7b6c7f760b6e0a447e43c6128bb52e402b
Hunter Stern <vonrosen2000@yahoo.com>
2019-09-01 12:32:30 -0700

    more changes

--- src/org/hunter/hackerrank/UniquePaths.java
+++ src/org/hunter/hackerrank/UniquePaths.java
@@ -1,9 +1,5 @@
 package org.hunter.hackerrank;
 
-import java.awt.Point;
-import java.util.HashMap;
-import java.util.Map;
-
 public class UniquePaths {
 
     public static void main(String [] args) {
@@ -17,64 +13,20 @@ public class UniquePaths {
     //m=across, n=down
     public int uniquePaths(int m, int n) {
 
-        if (m == 1 && n == 1) {
-            return 1;
-        }
-
-        Map<Point, Integer> map = new HashMap<Point, Integer>();
-
         int [][] mem = new int[m][n];
 
         for (int i = 0; i < mem.length; ++i) {
             for (int j = 0; j < mem[i].length; ++j) {
-                for (int k = 0; k < 2; ++k) {
-                    if (k == 0) { //move right
-                        if (i == 0 && j == 0) {
-                            if (i < mem.length - 1) {
-                                mem[i + 1][j] = 1;
-                                Point p = new Point(i + 1, j);
-
-                                map.put(p, 1);
-                            }
-                        }
-                        else {
-                            if (mem[i][j] > 0) {
-                                if (i < mem.length - 1) {
-                                    Integer current = map.get(new Point(i, j));
-                                    Integer next = map.get(new Point(i + 1, j));
-                                    mem[i + 1][j] = 1;
-
-                                    map.put(new Point(i + 1, j), next == null ? current : current + next);
-                                }
-                            }
-                        }
-                    }
-                    else { //move down
-                        if (i == 0 && j == 0) {
-                            if (j < mem[i].length - 1) {
-                                mem[i][j + 1] = 1;
-                                Point p = new Point(i, j + 1);
-                                map.put(p, 1);
-                            }
-                        }
-                        else {
-                            if (mem[i][j] > 0) {
-                                if (j < mem[i].length - 1) {
-                                    Integer current = map.get(new Point(i, j));
-                                    Integer next = map.get(new Point(i, j + 1));
-
-                                    mem[i][j + 1] = 1;
-
-                                    map.put(new Point(i, j + 1), next == null ? current : current + next);
-                                }
-                            }
-                        }
-                    }
+                if (i == 0 || j == 0) {
+                    mem[i][j] = 1;
+                }
+                else {
+                    mem[i][j] = mem[i - 1][j] + mem[i][j - 1];
                 }
             }
         }
 
-        return map.get(new Point(m - 1, n - 1)) == null ? 0 : map.get(new Point(m - 1, n - 1));
+        return mem[m - 1][n - 1];
     }
 
 }
--- src/org/hunter/hackerrank/UniquePaths2.java
+++ src/org/hunter/hackerrank/UniquePaths2.java
@@ -1,9 +1,5 @@
 package org.hunter.hackerrank;
 
-import java.awt.Point;
-import java.util.HashMap;
-import java.util.Map;
-
 public class UniquePaths2 {
 
     public static void main(String [] args) {
@@ -13,83 +9,39 @@ public class UniquePaths2 {
             {0,1,0},
             {0,0,0}
         }));
+        System.out.println(u.uniquePathsWithObstacles(new int[][] {
+            {1}
+        }));
+        System.out.println(u.uniquePathsWithObstacles(new int[][] {
+            {1,0}
+        }));
     }
 
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-        if (obstacleGrid.length == 1 && obstacleGrid[0].length == 1) {
-            return obstacleGrid[0][0] == 0 ? 1 : 0;
-        }
-
-
-        Map<Point, Integer> map = new HashMap<Point, Integer>();
-
         int [][] mem = new int[obstacleGrid.length][obstacleGrid[0].length];
 
         for (int i = 0; i < mem.length; ++i) {
             for (int j = 0; j < mem[i].length; ++j) {
-                mem[i][j] = obstacleGrid[i][j];
-            }
-        }
-
-        for (int i = 0; i < mem.length; ++i) {
-            for (int j = 0; j < mem[i].length; ++j) {
-                for (int k = 0; k < 2; ++k) {
-                    if (k == 0) { //move right
-                        if (i == 0 && j == 0) {
-                            if (i < mem.length - 1 && obstacleGrid[i][j] == 0) {
-                                if (obstacleGrid[i + 1][j] == 0) {
-                                    mem[i + 1][j] = 1;
-                                    Point p = new Point(i + 1, j);
-
-                                    map.put(p, 1);
-                                }
-                            }
-                        }
-                        else {
-                            if (mem[i][j] > 0) {
-                                if (i < mem.length - 1 && obstacleGrid[i][j] == 0) {
-                                    if (obstacleGrid[i + 1][j] == 0) {
-                                        Integer current = map.get(new Point(i, j));
-                                        Integer next = map.get(new Point(i + 1, j));
-                                        mem[i + 1][j] = 1;
-
-                                        map.put(new Point(i + 1, j), next == null ? current : current + next);
-                                    }
-                                }
-                            }
-                        }
+                if ((i == 0 || j == 0) && obstacleGrid[i][j] == 0) {
+                    if (i > 0) {
+                        mem[i][j] = obstacleGrid[i - 1][j] == 0 ? mem[i - 1][j] : 0;
                     }
-                    else { //move down
-                        if (i == 0 && j == 0) {
-                            if (j < mem[i].length - 1 && obstacleGrid[i][j] == 0) {
-                                if (obstacleGrid[i][j + 1] == 0) {
-                                    mem[i][j + 1] = 1;
-                                    Point p = new Point(i, j + 1);
-                                    map.put(p, 1);
-                                }
-                            }
-                        }
-                        else {
-                            if (mem[i][j] > 0) {
-                                if (j < mem[i].length - 1 && obstacleGrid[i][j] == 0) {
-                                    if (obstacleGrid[i][j + 1] == 0) {
-                                        Integer current = map.get(new Point(i, j));
-                                        Integer next = map.get(new Point(i, j + 1));
-
-                                        mem[i][j + 1] = 1;
-
-                                        map.put(new Point(i, j + 1), next == null ? current : current + next);
-                                    }
-                                }
-                            }
-                        }
+                    else if (j > 0) {
+                        mem[i][j] = obstacleGrid[i][j - 1] == 0 ? mem[i][j - 1] : 0;
+                    }
+                    else {
+                        mem[i][j] = 1;
+                    }
+                }
+                else {
+                    if (obstacleGrid[i][j] == 0) {
+                        mem[i][j] = mem[i - 1][j]  + mem[i][j - 1];
                     }
                 }
             }
         }
 
-        return map.get(new Point(mem.length - 1, mem[0].length - 1)) == null ? 0 :
-            map.get(new Point(mem.length - 1, mem[0].length - 1));
+        return mem[mem.length - 1][mem[0].length - 1];
     }
 
 }
