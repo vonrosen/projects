b46d5d83ea018926e85c589e0b1db97afb50c562
Hunter Stern <vonrosen2000@yahoo.com>
2019-09-08 12:24:40 -0700

    more changes

--- src/org/hunter/leetcode/LCA.java
+++ src/org/hunter/leetcode/LCA.java
@@ -148,4 +148,29 @@ expected=3
         }
     }
 
+    /**better solution **/
+    public TreeNode lowestCommonAncestorBetter(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) {
+            return null;
+        }
+        if (root == p || root == q) {
+            return root;
+        }
+
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if (left != null && right != null) {
+            return root;
+        }
+        else if (left != null && right == null) {
+            return left;
+        }
+        else if (left == null && right != null) {
+            return right;
+        }
+        else {
+            return null;
+        }
+
+    }
 }
