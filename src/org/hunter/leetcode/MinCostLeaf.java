3c9f2e45c5a3b3f347438f681bb19fec5f66c9e2
Hunter Stern <hunter.stern@autodesk.com>
2019-12-13 16:11:49 -0800

    more changes

--- src/org/hunter/leetcode/MinCostLeaf.java
+++ src/org/hunter/leetcode/MinCostLeaf.java
@@ -20,6 +20,103 @@ public class MinCostLeaf {
 		if (arr.length == 0 || arr.length == 1) {
 			return 0;
 		}
+
+		if (arr.length == 2) {
+			return arr[0] * arr[1];
+		}
+		
+		int result = 0;
+
+		List<int []> l = new ArrayList<int []>();
+		
+		int start = 0;
+		int end = 1;
+		int skip = 1;
+
+		for (; end < arr.length; end += skip) {
+			int prod = arr[start] * arr[end];
+			int max = Math.max(arr[start], arr[end]);
+			int left = -1;
+			if (start - 1 >= 0) {
+				left = start - 1;
+			}
+			
+			int right = -1;
+			if (end + 1 < arr.length) {
+				right = end + 1;
+			}
+			
+			if (left != -1) {
+				l.add(new int [] { prod, max, left, -1 });	
+			}
+			
+			if (right != -1) {
+				l.add(new int [] { prod, max, -1, right });
+			}
+						
+			start += skip;			
+		}
+
+		while (l.size() > 0) {
+			int minProd = 0;
+			int size = l.size();
+			for (int i = 0;  i < l.size(); ++i) {
+				int [] e = l.get(i);
+				
+				int prod = e[0];
+				int max = e[1];
+				int left = e[2];
+				int right = e[3];
+				
+				if (left != -1) {
+					int newProd = prod + (max * arr[left]);
+					int newLeft = left - 1 >= 0 ? left - 1 : -1;		
+					int newMax = Math.max(arr[left], max);
+					
+					if (minProd == 0) {
+						minProd = newProd;
+					}
+					else {
+						minProd = Math.min(minProd, newProd);
+					}									
+					
+					if (newLeft != -1) {
+						l.add(new int [] { newProd, newMax, newLeft, right });	
+					}					
+				}
+				
+				if (right != -1) {
+					int newProd = prod + (max * arr[right]);
+					int newRight = right + 1 < arr.length ? right + 1 : -1;					
+					int newMax = Math.max(arr[right], max);
+					
+					if (minProd == 0) {
+						minProd = newProd;
+					}
+					else {
+						minProd = Math.min(minProd, newProd);
+					}														
+					
+					if (newRight != -1) {
+						l.add(new int [] { newProd, newMax, left, newRight });	
+					}					
+				}	
+			}
+			
+			for (int i = size - 1; i >= 0; --i) {
+				l.remove(i);
+			}
+			
+			result = minProd;
+		}
+		
+		return result;
+	}
+	
+	public int mctFromLeafValues2(int[] arr) {
+		if (arr.length == 0 || arr.length == 1) {
+			return 0;
+		}
 		
 		List<Integer> l = new ArrayList<Integer>();
 		
