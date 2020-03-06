a5a0d2edc0a5b73b9fa91387b20583c3f9aae219
Hunter Stern <hunter.stern@autodesk.com>
2020-03-06 09:23:21 -0800

    more changes

--- src/org/hunter/leetcode/LLInBT.java
+++ src/org/hunter/leetcode/LLInBT.java
@@ -22,38 +22,69 @@ class TreeNode {
 public class LLInBT {
 
 	public static void main(String[] args) {
-		ListNode head = new ListNode(1);
+//		[4,2,8]
+//				[1,4,4,null,2,2,null,1,null,6,8,null,null,null,null,1,3]
+		//true
+		ListNode head = new ListNode(4);
+		head.next = new ListNode(2);
+		head.next.next = new ListNode(8);
+		
 		TreeNode root = new TreeNode(1);
+		root.left = new TreeNode(4);		
+		root.left.right = new TreeNode(2);
+		root.left.right.left = new TreeNode(1);
+		root.right = new TreeNode(4);
+		root.right.left = new TreeNode(2);
+		root.right.left.left = new TreeNode(6);
+		root.right.left.right = new TreeNode(8);
+		root.right.left.right.left = new TreeNode(1);
+		root.right.left.right.right = new TreeNode(3);
 		
 		LLInBT l = new LLInBT();
 		System.out.println(l.isSubPath(head, root));
 	}
 
 	public boolean isSubPath(ListNode head, TreeNode root) {
-		return search(root, head);
+		return search(root, head, false);
 	}
 
-	public boolean search(TreeNode root, ListNode head) {
+	public boolean search(TreeNode root, ListNode head, boolean isMatching) {
 		if (root.val == head.val) {
 			if (head.next == null) {
 				return true;
 			}
 			
 			if (root.left != null) {
-				return search(root.left, head.next);	
+				boolean result = search(root.left, head.next, true);
+				if (result) {
+					return result;
+				}
 			}
 			
 			if (root.right != null) {
-				return search(root.right, head.next);	
+				boolean result = search(root.right, head.next, true);
+				if (result) {
+					return result;
+				}
 			}			
-		}		
+		}
+		
+		if (isMatching) {
+			return false;
+		}
 		
 		if (root.left != null) {
-			return search(root.left, head);
+			boolean result = search(root.left, head, false);
+			if (result) {
+				return result;
+			}
 		}
 		
 		if (root.right != null) {
-			return search(root.right, head);
+			boolean result = search(root.right, head, false);
+			if (result) {
+				return result;
+			}
 		}
 		
 		return false;
