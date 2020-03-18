1011c602399eeb93ae1db59e9aaa37dd3bb3d36e
Hunter Stern <hunter.stern@autodesk.com>
2020-03-17 17:55:01 -0700

    more changes

---
+++ src/org/hunter/Fibo2.java
@@ -0,0 +1,40 @@
+package org.hunter;
+
+public class Fibo2 {
+
+	public static void main(String [] args) {
+		Fibo2 f = new Fibo2();
+		
+		System.out.println(f.fibo(21));
+		System.out.println(f.fiboDp(21));
+	}
+	
+	//O(2^N)
+	public long fibo(int n) {
+		if (n == 1) {
+			return 0;
+		}
+		
+		if (n <= 3) {
+			return 1;
+		}
+		
+		return fibo(n - 1) + fibo(n - 2);
+	}
+	
+	//O(N)
+	public long fiboDp(int n) {
+		long [] mem = new long[n + 1];
+		
+		mem[1] = 0;
+		mem[2] = 1;
+		mem[3] = 1;
+		
+		for (int i = 4; i <= n; ++i) {			
+			mem[i] = mem[i - 1] + mem[i - 2];			
+		}
+		
+		return mem[n];		
+	}	
+	
+}
