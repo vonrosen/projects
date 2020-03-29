8481e63d4c4533a680975260890c23eb87abab0a
Hunter Stern <vonrosen2000@yahoo.com>
2020-03-29 16:03:30 -0700

    more changes

--- src/org/hunter/MergeSort2.java
+++ src/org/hunter/MergeSort2.java
@@ -3,33 +3,26 @@ package org.hunter;
 public class MergeSort2 {
 
 	public static void main(String [] args) {
-		int [] arr = new int [] {
-				4,
-				44,
-				22,
-				11,
-				23,
-				8				
-		};		
-		MergeSort2 m = new MergeSort2();		
-		int [] sorted = m.sort(arr, 0, arr.length - 1);		
+		int [] arr = new int [] {5, 3, 2, 1, 4, 6};
+		MergeSort2 m = new MergeSort2();
+		int [] sorted = m.sort(arr, 0, arr.length - 1);
 		for (int i = 0; i < sorted.length; ++i) {
 			System.out.println(sorted[i]);
 		}
 	}
-	
+
 	public int [] sort(int [] arr, int start, int end) {
 		if (start >= end) {
 			return new int [] { arr[end] };
 		}
-		
+
 		int mid = start + ((end - start) / 2);
-		
+
 		int [] arr1 = sort(arr, start, mid);
 		int [] arr2 = sort(arr, mid + 1, end);
 
 		int [] newArr = new int[arr1.length + arr2.length];
-		
+
 		int left = 0;
 		int right = 0;
 		for (int i = 0; i < newArr.length; ++i) {
@@ -38,19 +31,19 @@ public class MergeSort2 {
 				++right;
 				continue;
 			}
-			
+
 			if (right >= arr2.length) {
 				newArr[i] = arr1[left];
 				++left;
 				continue;
 			}
-			
+
 			if (arr1[left] < arr2[right]) {
 				newArr[i] = arr1[left];
 				++left;
 				continue;
 			}
-			
+
 			if (arr1[left] > arr2[right]) {
 				newArr[i] = arr2[right];
 				++right;
@@ -60,5 +53,5 @@ public class MergeSort2 {
 
 		return newArr;
 	}
-	
+
 }
---
+++ src/org/hunter/leetcode/KthSmallest.java
@@ -0,0 +1,95 @@
+package org.hunter.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
+
+public class KthSmallest {
+
+    public static void main(String[] args) {
+//        TreeNode t = new TreeNode(3);
+//        t.left = new TreeNode(1);
+//        t.right = new TreeNode(4);
+//        t.left.right = new TreeNode(2);
+//
+//        int k = 1;
+
+//        [5,3,6,2,4,null,null,1]
+//                3
+        //output: 3
+
+        TreeNode t = new TreeNode(5);
+        t.left = new TreeNode(3);
+        t.right = new TreeNode(6);
+        t.left.left = new TreeNode(2);
+        t.left.right = new TreeNode(4);
+        t.left.left.left = new TreeNode(1);
+        int k = 4;
+
+        KthSmallest o = new KthSmallest();
+        System.out.println(o.kthSmallest(t, k));
+    }
+
+    public int kthSmallest(TreeNode root, int k) {
+        List<Integer> values = new ArrayList<Integer>();
+
+        populateValuesList(root, values);
+        sort(values, 0, values.size() - 1);
+
+        return values.get(k - 1);
+    }
+
+    public void sort(List<Integer> values, int start, int end) {
+        if (start < end) {
+            int newMid = partition(values, start, end);
+            sort(values, start, newMid - 1);
+            sort(values, newMid + 1, end);
+        }
+    }
+
+    public int partition(List<Integer> values, int start, int end) {
+        int lower = start;
+
+        for (int i = start; i <= end; ++i) {
+            if (values.get(i) < values.get(end)) {
+                swap(values, lower, i);
+                ++lower;
+            }
+        }
+
+        swap(values, lower > end ? end : lower, end);
+        return lower > end ? end : lower;
+    }
+
+    public void swap(List<Integer> values, int index1, int index2) {
+        int tmp = values.get(index1);
+        values.set(index1, values.get(index2));
+        values.set(index2, tmp);
+    }
+
+    public void populateValuesList(TreeNode root, List<Integer> values) {
+        if (root == null) {
+            return;
+        }
+
+        values.add(root.val);
+
+        if (root.left != null) {
+            populateValuesList(root.left, values);
+        }
+
+        if (root.right != null) {
+            populateValuesList(root.right, values);
+        }
+    }
+
+}
