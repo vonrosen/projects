f981370b7bb51241d89558837f3661b592db55dd
Hunter Stern <hunter.stern@autodesk.com>
2020-04-30 18:49:08 -0700

    more changes

--- src/org/hunter/leetcode/LongestCommonSubsequence.java
+++ src/org/hunter/leetcode/LongestCommonSubsequence.java
@@ -13,8 +13,19 @@ public class LongestCommonSubsequence {
 //		String text1 = "abc", text2 = "abc";
 //		String text1 = "abc", text2 = "def";
 		
-		String text1 = "pmjghexybyrgzczy";
-		String text2 = "hafcdqbgncrcbihkd";		
+//		String text1 = "pmjghexybyrgzczy";
+//		String text2 = "hafcdqbgncrcbihkd";
+		//4
+		
+//		String text1 = "bsbininm";
+//		String text2 = "jmjkbkjkv";
+		
+//		String text1 = "yzebsbuxmtcfmtodclszgh";
+//		String text2 = "ejevmhcvshclydqrulwbyha";
+		
+		String text1 = "mhunuzqrkzsnidwbun";
+		String text2 = "szulspmhwpazoxijwbq";
+		//6
 		
 		LongestCommonSubsequence l = new LongestCommonSubsequence();
 		System.out.println(l.longestCommonSubsequence(text1, text2));
@@ -22,6 +33,8 @@ public class LongestCommonSubsequence {
 	
 	public int longestCommonSubsequence(String text1, String text2) {
 		int maxSize = 0;		
+		int lastMatchPos = text1.length();
+		int matchPos = 0;
 		Map<Integer, List<String>> mem = new HashMap<Integer, List<String>>();
 		mem.put(text1.length(), new ArrayList<String>());
 		for (int i = text1.length() - 1; i >= 0; --i) {			
@@ -30,41 +43,42 @@ public class LongestCommonSubsequence {
 			int io = text2.indexOf(charAtThisPos);
 			if (io > -1) {
 				maxSize = Math.max(1, maxSize);
+				mem.get(i).add(String.valueOf(charAtThisPos));
+				matchPos = i;
 			}			
-			mem.get(i).add(String.valueOf(charAtThisPos));
-			for (String s : mem.get(i + 1)) {
+			else {
+				continue;
+			}						
+			for (String s : mem.get(lastMatchPos)) {
 				StringBuffer sb = new StringBuffer();
 				sb.append(charAtThisPos).append(s);
-				maxSize = Math.max(textLen(sb.toString(), text2), maxSize);
-				mem.get(i).add(sb.toString());
-				mem.get(i).add(s);
-			}			
-			
-			
-			
-//			for (int p = i + 1; p < text1.length(); ++p) {				
-//				for (String s : mem.get(p)) {
-//					StringBuffer sb = new StringBuffer();
-//					sb.append(charAtThisPos).append(s);
-//					maxSize = Math.max(textLen(sb.toString(), text2), maxSize);
-//					mem.get(i).add(sb.toString());
-//				}
-//			}
+				int len = textLen(sb.toString(), text2);
+				if (len != -1) {
+					maxSize = Math.max(textLen(sb.toString(), text2), maxSize);
+					mem.get(i).add(sb.toString());
+					mem.get(i).add(s);
+				}
+			}
+			lastMatchPos = matchPos;
 		}
 		
 		return maxSize;
-    }
+    }	
 	
 	public int textLen(String subString, String text) {
 		int pos = -1;
 		for (char aChar : subString.toCharArray()) {
-			int textPos = text.lastIndexOf(aChar);
-			if (textPos > pos) {
-				pos = textPos;				
-			}
-			else {
+			int pos2 = text.indexOf(aChar, pos);
+			if (pos2 == -1) {
 				return -1;
 			}
+			while (pos2 < pos && pos2 != -1) {
+				pos2 = text.indexOf(aChar, pos2);
+			}
+			if (pos2 == -1) {
+				return -1;
+			}
+			pos = pos2 + 1;
 		}
 		
 		return subString.length();
