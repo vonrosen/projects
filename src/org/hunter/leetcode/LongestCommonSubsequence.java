d3616e75a0c13374848da139f31c31374422c6fa
Hunter Stern <hunter.stern@autodesk.com>
2020-06-02 16:17:45 -0700

    more changes

--- src/org/hunter/leetcode/LongestCommonSubsequence.java
+++ src/org/hunter/leetcode/LongestCommonSubsequence.java
@@ -28,13 +28,17 @@ public class LongestCommonSubsequence {
 //		String text2 = "hafcdqbgncrcbihkd";
 		//4
 
+//		String text1 = "eeeee";
+//		String text2 = "eeeee";
+
+
 //		String text1 = "dknkdizqxkdczafixidorgfcnkrirmhmzqbcfuvojsxwraxe";
 //		String text2 = "dulixqfgvipenkfubgtyxujixspoxmhgvahqdmzmlyhajerqz";
 		//14
 
 		String text1 = "fcvafurqjylclorwfoladwfqzkbebslwnmpmlkbezkxoncvwhstwzwpqxqtyxozkpgtgtsjobujezgrkvevklmludgtyrmjaxyputqbyxqvupojutsjwlwluzsbmvyxifqtglwvcnkfsfglwjwrmtyxmdgjifyjwrsnenuvsdedsbqdovwzsdghclcdexmtsbexwrszihcpibwpidixmpmxshwzmjgtadmtkxqfkrsdqjcrmxkbkfoncrcvoxuvcdytajgfwrcxivixanuzerebuzklyhezevonqdsrkzetsrgfgxibqpmfuxcrinetyzkvudghgrytsvwzkjulmhanankxqfihenuhmfsfkfepibkjmzybmlkzozmluvybyzsleludsxkpinizoraxonmhwtkfkhudizepyzijafqlepcbihofepmjqtgrsxorunshgpazovuhktatmlcfklafivivefyfubunszyvarcrkpsnglkduzaxqrerkvcnmrurkhkpargvcxefovwtapedaluhclmzynebczodwropwdenqxmrutuhehadyfspcpuxyzodifqdqzgbwhodcjonypyjwbwxepcpujerkrelunstebopkncdazexsbezmhynizsvarafwfmnclerafejgnizcbsrcvcnwrolofyzulcxaxqjqzunedidulspslebifinqrchyvapkzmzwbwjgbyrqhqpolwjijmzyduzerqnadapudmrazmzadstozytonuzarizszubkzkhenaxivytmjqjgvgzwpgxefatetoncjgjsdilmvgtgpgbibexwnexstipkjylalqnupexytkradwxmlmhsnmzuxcdkfkxyfgrmfqtajatgjctenqhkvyrgvapctqtyrufcdobibizihuhsrsterozotytubefutaxcjarknynetipehoduxyjstufwvkvwvwnuletybmrczgtmxctuny";
 		String text2 = "nohgdazargvalupetizezqpklktojqtqdivcpsfgjopaxwbkvujilqbclehulatshehmjqhyfkpcfwxovajkvankjkvevgdovazmbgtqfwvejczsnmbchkdibstklkxarwjqbqxwvixavkhylqvghqpifijohudenozotejoxavkfkzcdqnoxydynavwdylwhatslyrwlejwdwrmpevmtwpahatwlaxmjmdgrebmfyngdcbmbgjcvqpcbadujkxaxujudmbejcrevuvcdobolcbstifedcvmngnqhudixgzktcdqngxmruhcxqxypwhahobudelivgvynefkjqdyvalmvudcdivmhghqrelurodwdsvuzmjixgdexonwjczghalsjopixsrwjixuzmjgxydqnipelgrivkzkxgjchibgnqbknstspujwdydszohqjsfuzstyjgnwhsrebmlwzkzijgnmnczmrehspihspyfedabotwvwxwpspypctizyhcxypqzctwlspszonsrmnyvmhsvqtkbyhmhwjmvazaviruzqxmbczaxmtqjexmdudypovkjklynktahupanujylylgrajozobsbwpwtohkfsxeverqxylwdwtojoxydepybavwhgdehafurqtcxqhuhkdwxkdojipolctcvcrsvczcxedglgrejerqdgrsvsxgjodajatsnixutihwpivihadqdotsvyrkxehodybapwlsjexixgponcxifijchejoxgxebmbclczqvkfuzgxsbshqvgfcraxytaxeviryhexmvqjybizivyjanwxmpojgxgbyhcruvqpafwjslkbohqlknkdqjixsfsdurgbsvclmrcrcnulinqvcdqhcvwdaxgvafwravunurqvizqtozuxinytafopmhchmxsxgfanetmdcjalmrolejidylkjktunqhkxchyjmpkvsfgnybsjedmzkrkhwryzan";
-
+		//323
 		LongestCommonSubsequence l = new LongestCommonSubsequence();
 		System.out.println(l.longestCommonSubsequence(text1, text2));
 	}
@@ -62,121 +66,55 @@ public class LongestCommonSubsequence {
 		}
 	}
 
+	int calls = 0;
+	//18609502
+
 	public int longestCommonSubsequence(String text1, String text2) {
-		Map<CacheKey, Integer> mem = new HashMap<CacheKey, Integer>();
-		return longestCommonSubsequenceMem(text1, text2, null, null, mem);
+		Map<String, Integer> mem = new HashMap<String, Integer>();
+
+		StringBuffer t1 = new StringBuffer();
+		StringBuffer t2 = new StringBuffer();
+		for (char ch : text1.toCharArray()) {
+			if (text2.charAt(ch) != -1) {
+				t1.append(ch);
+			}
+		}
+		for (char ch : text2.toCharArray()) {
+			if (text1.charAt(ch) != -1) {
+				t2.append(ch);
+			}
+		}
+		return longestCommonSubsequenceMem(t1.toString(), t2.toString(),0, 0, mem);
 	}
 
-	public int longestCommonSubsequenceMem(String text1, String text2, List<String []> stringToPos, List<String[]> stringToPos2, Map<CacheKey, Integer> mem) {
-		if (text1.length() == 0 || text2.length() == 0) {
+	public int longestCommonSubsequenceMem(String text1, String text2, int start, int start2, Map<String, Integer> mem) {
+		if (start >= text1.length() || start2 >= text2.length()) {
+			mem.put(start + "-" + start2, 0);
 			return 0;
 		}
-		if (text1.equals(text2)) {
-			return text1.length();
-		}
-		if (text1.length() == 1 && text2.length() == 1) {
-			if (text1.equals(text2)) {
-				return 1;
-			}
-			return 0;
-		}
-
-		if (stringToPos == null) {
-			stringToPos = new ArrayList<String[]>();
-			for (int i = 0; i < text1.length(); ++i) {
-				stringToPos.add(new String [] { String.valueOf(text1.charAt(i)), String.valueOf(i) });
-			}
-		}
-
-		if (stringToPos2 == null) {
-			stringToPos2 = new ArrayList<String []>();
-			for (int i = 0; i < text2.length(); ++i) {
-				stringToPos2.add(new String [] { String.valueOf(text2.charAt(i)), String.valueOf(i) });
-			}
-		}
-
 		int maxSize = 0;
-		int index = 0;
-		int pos = 0;
-		Map<Integer, String> matches = new HashMap<Integer, String>();
-		Map<Integer, Integer> positions = new HashMap<Integer, Integer>();
-		for (char aChar : text1.toCharArray()) {
-			int indexOf = text2.indexOf(aChar);
-			if (indexOf > -1) {
-				maxSize = Math.max(1, maxSize);
-				matches.put(index,  String.valueOf(aChar));
-				positions.put(index, Integer.parseInt(stringToPos.get(pos)[1]));
-				++index;
-			}
-			++pos;
-		}
-		Map<Integer, String> matches2 = new HashMap<Integer, String>();
-		Map<Integer, Integer> positions2 = new HashMap<Integer, Integer>();
-		index = 0;
-		pos = 0;
-		for (char aChar : text2.toCharArray()) {
-			int indexOf = text1.indexOf(aChar);
-			if (indexOf > -1) {
-				maxSize = Math.max(1, maxSize);
-				matches2.put(index,  String.valueOf(aChar));
-				positions2.put(index, Integer.parseInt(stringToPos2.get(pos)[1]));
-				++index;
-			}
-			++pos;
-		}
+		int pos1 = start;
+		for (char char1 : text1.substring(start).toCharArray()) {
+			String string = String.valueOf(char1);
 
-		for (int i = 0; i < matches.size(); ++i) {
-			String string = matches.get(i);
-			Integer pos1 = positions.get(i);
-
-			for (int j = 0; j < matches2.size(); ++j) {
-				String string2 = matches2.get(j);
-				Integer pos2 = positions2.get(j);
-
-				if (mem.get(new CacheKey(pos1, pos2)) != null) {
-					maxSize = Math.max(maxSize, mem.get(new CacheKey(pos1, pos2)));
-					continue;
-				}
+			int pos2 = start2;
+			for (char char2 : text2.substring(start2).toCharArray()) {
+				String string2 = String.valueOf(char2);
 
 				if (string.equals(string2)) {
-					List<String> after1 = new ArrayList<String>();
-					List<String> after2 = new ArrayList<String>();
-					for (int l = i + 1; l < matches.size(); ++l) {
-						after1.add(matches.get(l));
-					}
-					for (int k = j + 1; k < matches2.size(); ++k) {
-						after2.add(matches2.get(k));
-					}
-					StringBuffer after1Matches = new StringBuffer();
-					StringBuffer after2Matches = new StringBuffer();
-					List<String []> newStringToPos = new ArrayList<String []>();
-					List<String []> newStringToPos2 = new ArrayList<String []>();
-					pos = i + 1;
-					for (String s : after1) {
-						for (String s2 : after2.stream().distinct().collect(Collectors.toList())) {
-							if (s.equals(s2)) {
-								after1Matches.append(s);
-								newStringToPos.add(new String [] { s, String.valueOf(positions.get(pos)) });
-							}
-						}
-						++pos;
-					}
-					pos = j + 1;
-					for (String s : after2) {
-						for (String s2 : after1.stream().distinct().collect(Collectors.toList())) {
-							if (s.equals(s2)) {
-								after2Matches.append(s);
-								newStringToPos2.add(new String [] { s, String.valueOf(positions2.get(pos)) });
-							}
-						}
-						++pos;
-					}
+					longestCommonSubsequenceMem(text1, text2, pos1 + 1, pos2 + 1, mem);
+					
 
-					int size = 1 + longestCommonSubsequenceMem(after1Matches.toString(), after2Matches.toString(), newStringToPos, newStringToPos2, mem);
-					mem.put(new CacheKey(pos1, pos2), size);
-					maxSize = Math.max(size, maxSize);
+
+					String key = (pos1 + 1) + "-" + (pos2 + 1);
+					int lastSize = mem.get(key) == null ? longestCommonSubsequenceMem(text1, text2, pos1 + 1, pos2 + 1, mem) : mem.get(key);
+					int size = 1 + lastSize;
+					mem.put(pos1 + "-" + pos2, size);
+					maxSize = Math.max(maxSize, size);
 				}
+				++pos2;
 			}
+			++pos1;
 		}
 
 		return maxSize;
