f68060778a494150a983cd53a96033584a4d00d3
Hunter Stern <hunter.stern@autodesk.com>
2020-06-17 08:38:11 -0700

    more changes

--- src/org/hunter/leetcode/LongestCommonSubsequence.java
+++ src/org/hunter/leetcode/LongestCommonSubsequence.java
@@ -41,14 +41,7 @@ public class LongestCommonSubsequence {
 	}
 
 	public int longestCommonSubsequence(String text1, String text2) {
-		Map<String, Integer> mem = new LinkedHashMap<>();
-		Map<Integer, Integer> mem2 = new LinkedHashMap<>();
-		return longestCommonSubsequenceMem(text1, text2);
-	}
-
-	public int longestCommonSubsequenceMem(String text1, String text2) {
 		int [][] mem = new int[text1.length()][text2.length()];
-		Map<String, Integer> map = new HashMap<String, Integer>();
 		int pos1 = 0, maxSize = 0;
 		for (char char1 : text1.toCharArray()) {
 			String string = String.valueOf(char1);
@@ -58,30 +51,14 @@ public class LongestCommonSubsequence {
 				if (string.equals(string2)) {
 					if (pos1 == 0 || pos2 == 0) {
 						mem[pos1][pos2] = 1;
-						map.put(String.valueOf(pos1) + "-" + String.valueOf(pos2), 1);
 					}
 					else {
-						int last = map.get(String.valueOf(pos1 - 1) + "-" + String.valueOf(pos2 - 1));
-						mem[pos1][pos2] = 1 + last;
-						map.put(String.valueOf(pos1) + "-" + String.valueOf(pos2), mem[pos1][pos2]);
+						mem[pos1][pos2] = 1 + mem[pos1 - 1][pos2 - 1];
 					}
 					maxSize = Math.max(maxSize, mem[pos1][pos2]);
 				}
 				else {
-					Integer last1 = map.get(String.valueOf(pos1 - 1) + "-" + String.valueOf(pos2 - 1));
-					Integer last2 = map.get(String.valueOf(pos1 - 1) + "-" + String.valueOf(pos2));
-					Integer last3 = map.get(String.valueOf(pos1) + "-" + String.valueOf(pos2 - 1));
-					if (last1 == null) {
-						last1 = 0;
-					}
-					if (last2 == null) {
-						last2 = 0;
-					}
-					if (last3 == null) {
-						last3 = 0;
-					}
-					int max = Math.max(Math.max(last1, last2), last3);
-					map.put(String.valueOf(pos1) + "-" + String.valueOf(pos2), max);
+					mem[pos1][pos2] = Math.max(mem[pos1][pos2 > 0 ? pos2 - 1 : pos2], mem[pos1 > 0 ? pos1 - 1 : pos1][pos2]);
 				}
 				++pos2;
 			}
