a2debcac4374024514b5d1902279ef145526f930
hunterst <hunterst@zillowgroup.com>
2020-07-28 14:02:23 -0700

    more changes

--- src/org/hunter/Primes.java
+++ src/org/hunter/Primes.java
@@ -13,14 +13,8 @@ public class Primes{
 	public static void primes(int n) {
 		List<Integer> previous = new ArrayList<>();
 		Stream.iterate(1, i -> i += 2).filter(i -> {
-			boolean isPrime;
-			if (previous.stream().anyMatch(p -> i % p == 0)) {
-				isPrime = false;
-			}
-			else {
-				isPrime = true;
-			}
-			if (i != 1) {
+			boolean isPrime = !previous.stream().anyMatch(p -> i % p == 0);
+			if (i != 1 && isPrime) {
 				previous.add(i);
 			}
 			return isPrime;
